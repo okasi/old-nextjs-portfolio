f4f6bdd313be6fec3a07bcc18a261e39d88ab228
okan-s <okan.simsek@chasacademy.se>
2019-06-05 03:36:53 +0200

    :tada:

---
+++ src/routes/about.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const About = () => {
+}
+
+export default About;
---
+++ src/routes/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <center>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1 style={{color: 'white'}}>Hello from home</h1>
+        </center>
+    )
+}
+
+export default Home;
